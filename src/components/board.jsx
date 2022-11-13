21df9f74cfa0a8521cca0e7f2765e5f7f7e6e33f
Arun <jangirarun786@gmail.com>
2022-11-13 03:03:12 +0300

    Working Version 1

---
+++ src/components/board.jsx
@@ -0,0 +1,92 @@
+// This is an SVG element where all the drawing will happen
+// based on the JSON Tree Structure
+import React, { Component } from "react";
+import Disc from "./disc";
+import genome from "../data/genome.json";
+import setting from "../data/setting.json";
+// import CurvedText from "./curvedText";
+class Board extends Component {
+    state = {
+        members: this.loadData2(),
+    };
+
+    loadData2() {
+        const persons = genome.persons;
+        const { rootId, gens } = setting;
+        return this.getGenerations(persons, rootId, gens);
+    }
+
+    getPersonById(persons, id) {
+        for (let i = 0; i < persons.length; i++) {
+            const person = persons[i];
+            if (person.id === id) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    getGenerations(persons, rootId, maxlevel = 1) {
+        let level = 0;
+        let currentNode = this.getPersonById(persons, rootId);
+        // console.log(currentNode)
+        let queue = [];
+        let members = [];
+        queue.push(currentNode);
+
+        while (queue.length && level < maxlevel) {
+            let level_size = queue.length;
+            let num = 0;
+            while (num < level_size) {
+                currentNode = queue.shift();
+                // console.log(currentNode.id)
+                members.push({
+                    id: currentNode.id,
+                    name: currentNode.name,
+                    discParams: {
+                        id: currentNode.id,
+                        rl: 150 + 50 * level,
+                        rh: 200 + 50 * level,
+                        theta: 180 / level_size,
+                        phi: -(num * 180) / level_size,
+                        x: 0,
+                        y: 0,
+                    },
+                });
+
+                // members.push(currentNode.name);
+                if (currentNode.relationships) {
+                    currentNode.relationships.forEach((relation) => {
+                        relation.children.forEach((id) => {
+                            queue.push(this.getPersonById(persons, id));
+                        });
+                    });
+                }
+                num++;
+            }
+            level++;
+        }
+        console.log(members);
+        return members;
+    }
+
+    render() {
+        // console.log(this.getGenerations(genome.persons, setting.rootId));
+        const { members } = this.state;
+        return (
+            <svg width="100%" height="100vh" >
+                <g transform="translate(750 600)">
+                    {members.map((member) => (
+                        <Disc
+                            key={member.id}
+                            discParams={member.discParams}
+                            name={member.name}
+                        />
+                    ))}
+                </g>
+            </svg>
+        );
+    }
+}
+
+export default Board;
--- src/components/disc.jsx
+++ src/components/disc.jsx
@@ -1,48 +1,61 @@
 import React, { Component } from "react";
 class Disc extends Component {
-    state = {
-        rl: 0,
-        rh: 250,
-        theta: 45, // deg
-        phi: -25, //deg
-        x: 100,
-        y: 100,
+    state = {};
+
+    onClick = () => {
+        console.log(this.props.name);
     };
     render() {
-        const { rl, rh, theta: th, phi, x, y } = this.state;
+        const { rl, rh, theta: th, phi, x, y, id } = this.props.discParams;
+        const { name } = this.props;
         const { sin, cos, PI } = Math;
         const deg = PI / 180;
-        const p2 = [rl * cos(th * deg), rl * sin(th * deg)];
-        const p3 = [rh * cos(th * deg), rh * sin(th * deg)];
-        const d = `M${rl},0  A${rl},${rl} 0 0 1 ${p2[0]},${p2[1]}, L${p3[0]} ${p3[1]} A${rh} ${rh} 0,0,0, ${rh} 0z`;
+        const p2 = [rl * cos(th * deg), -rl * sin(th * deg)];
+        const p3 = [rh * cos(th * deg), -rh * sin(th * deg)];
+        const p4 = [
+            (rl + rh) * 0.5 * cos(th * deg),
+            -(rl + rh) * 0.5 * sin(th * deg),
+        ];
+        const d = `M${rl},0  A${rl},${rl} 0 0 0 ${p2[0]},${p2[1]}, L${p3[0]} ${p3[1]} A${rh} ${rh} 0,0,1, ${rh} 0z`;
         return (
-            <div height={"100vh"}>
-                <svg width={"100%"} height={"100vh"}>
-                    <rect
-                        x={0}
-                        y={0}
-                        width={"100%"}
-                        height={"100%"}
-                        fill="gray"
-                    ></rect>
-                    <rect
-                        x={0}
-                        y={0}
-                        width={"100"}
-                        height={"100"}
-                        fill="black"
-                    ></rect>
-                    <g transform={`translate(${x} ${y}) rotate(${phi} 0 0)`}>
-                        <path
-                            d={d}
-                            fill="red"
-                            strokeWidth="1"
-                            stroke="blue"
-                            cursor="pointer"
-                        />
-                    </g>
-                </svg>
-            </div>
+            <g>
+                <g transform={`translate(${x} ${y}) rotate(${phi} 0 0)`}>
+                    <path
+                        d={d}
+                        fill="#F7A4A4"
+                        strokeWidth="5"
+                        stroke="white"
+                        cursor="pointer"
+                        title={name}
+                        onClick={this.onClick}
+                    />
+                </g>
+
+                <g transform={`translate(${x} ${y}) rotate(${phi} 0 0)`}>
+                    <path
+                        d={`M ${p4[0]},${p4[1]}   A${(rl + rh) * 0.5},${
+                            (rl + rh) * 0.5
+                        } 0 0 1 ${(rl + rh) * 0.5},0`}
+                        stroke="black"
+                        strokeWidth={0}
+                        id={id}
+                        fill="None"
+                    ></path>
+                    <text
+                        width="500"
+                        dominantBaseline="middle"
+                        textAnchor="middle"
+                    >
+                        <textPath
+                            startOffset="50%"
+                            alignmentBaseline="bottom"
+                            href={`#${id}`}
+                        >
+                            {name}
+                        </textPath>
+                    </text>
+                </g>
+            </g>
         );
     }
 }
