4bbcd88dd037e48d3756f7c067f94fdf6721ac46
Arun <jangirarun786@gmail.com>
2022-12-05 18:28:22 +0300

    function component implementation to do

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 export const SIBLINGSEP = 1.0
 export const COUSINSEP = 1.1
-export const ROOT_ID = "e4383076-0591-aebd-0ab6-c46c368be839"
-export const NUM_GEN = 2
+export const _rootID = "e4383076-0591-aebd-0ab6-c46c368be839"
+export const _maxDepth = 2
 
 export const _R = 120
 export const _ST = 0.48 // Theta Multiplier
